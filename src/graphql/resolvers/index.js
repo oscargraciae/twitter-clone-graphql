d69269b375de953f08d9f9725a7a0ade02c5dd74
Oscar Gracia <oscar@gigbox.mx>
2017-10-09 16:17:37 -0500

    First commit API

---
+++ src/graphql/resolvers/index.js
@@ -0,0 +1,25 @@
+import GraphqlDate from 'graphql-date';
+
+import TweetResolvers from './tweet-resolvers';
+import UserResolvers from './user-resolvers';
+import User from '../../models/User';
+
+export default {
+  Date: GraphqlDate,
+  Tweet: {
+    user: ({ user }) => User.findById(user),
+  },
+  Query: {
+    getTweet: TweetResolvers.getTweet,
+    getTweets: TweetResolvers.getTweets,
+    getUserTweets: TweetResolvers.getUserTweets,
+    me: UserResolvers.me,
+  },
+  Mutation: {
+    createTweet: TweetResolvers.createTweet,
+    updateTweet: TweetResolvers.updateTweet,
+    deleteTweet: TweetResolvers.deleteTweet,
+    signup: UserResolvers.signup,
+    login: UserResolvers.login,
+  }
+};
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { createServer } from 'http';
+
+import './config/db';
+import constants from './config/constants';
+import middlewares from './config/middlewares';
+import mocks from './mocks';
+
+const app = express();
+
+middlewares(app);
+
+const graphQLServer = createServer(app);
+
+// mocks().then(() => {
+  app.listen(constants.PORT, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(`App listen to por ${constants.PORT}`);
+    }
+  });
+// });
